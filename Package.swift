567897816610ea6ea282303dd50ecd024629a029
Aimen <hdmen55@hotmail.com>
2026-03-28 02:00:21 -0400

    chore: bump to macOS 14, add swift-markdown + swift-snapshot-testing + test target

--- Package.swift
+++ Package.swift
@@ -4,13 +4,28 @@ import PackageDescription
 let package = Package(
     name: "FloatNote",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v14)
+    ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-markdown", from: "0.5.0"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.17.0")
     ],
     targets: [
         .executableTarget(
             name: "FloatNote",
+            dependencies: [
+                .product(name: "Markdown", package: "swift-markdown")
+            ],
             path: "Sources/FloatNote",
             exclude: ["Resources"]
+        ),
+        .testTarget(
+            name: "FloatNoteTests",
+            dependencies: [
+                "FloatNote",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ],
+            path: "Tests/FloatNoteTests"
         )
     ]
 )
---
+++ Tests/FloatNoteTests/PlaceholderTests.swift
@@ -0,0 +1,9 @@
+import Testing
+
+@Suite("Placeholder")
+struct PlaceholderTests {
+    @Test("project compiles with test target")
+    func compiles() {
+        #expect(true)
+    }
+}
